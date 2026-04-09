b45fdc45f3586cd957e9f4cb337d2de3ebc2157b
clydehoffer <isaiahcotton999@gmail.com>
2026-04-08 20:14:57 -0700

    Add files via upload

---
+++ categories.js
@@ -0,0 +1,7 @@
+const CATEGORIES = [
+  { id: "best-in-show",    name: "Best in Show" },
+  { id: "eagles-choice",   name: "Eagle's Choice" },
+  { id: "best-paint",      name: "Best Paint" },
+  { id: "best-motorcycle", name: "Best Motorcycle" },
+  { id: "best-student",    name: "Best Student Car" }
+];
